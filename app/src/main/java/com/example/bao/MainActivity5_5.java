e7659753019cc1fb4df9d57d60c39f6e00a406ac
Hoang Truong <62192868+0h59p@users.noreply.github.com>
2020-03-22 21:01:25 +0700

    first commit

---
+++ app/src/main/java/com/example/bao/MainActivity5_5.java
@@ -0,0 +1,25 @@
+package com.example.bao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity5_5 extends AppCompatActivity {
+    WebView webView;
+    private String dlink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        webView = findViewById(R.id.webview);
+        Intent intent = getIntent();
+        String dlink = intent.getStringExtra("link");
+        webView.loadUrl(dlink);
+        webView.setWebViewClient(new WebViewClient());
+    }
+}
